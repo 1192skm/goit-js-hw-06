b790925b239a05d901057e4db74279ef66aa57bd
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-03-21 22:23:51 +0200

    Task 4, 5

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-// const items = document.querySelector("#categories");
-// console.log(`Number of categories: ${items.children.length}`);
+const items = document.querySelector("#categories");
+console.log(`Number of categories: ${items.children.length}`);
 
-// [...items.children].forEach((num) => {
-//     console.log(`Categoty: ${num.firstElementChild.textContent}`);
-//     console.log(`Elements: ${num.lastElementChild.children.length}`);
-// });
+[...items.children].forEach((num) => {
+    console.log(`Categoty: ${num.firstElementChild.textContent}`);
+    console.log(`Elements: ${num.lastElementChild.children.length}`);
+});
